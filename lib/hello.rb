92c42c40d8a14e12bfbf04ca987ad7ab5a2de1d6
adeeba reaz <adeebarivera@outlook.com>
2020-08-08 17:02:22 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,10 +1,10 @@
 def hello_t
-["Tim", "Tom", "Jim"].each do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
+ i = 0
+ 
+  while i < array.length
+    i = i + 1
   end
 end
-
 
 # call your method here!
 
